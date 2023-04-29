6df1246eeb2a86c0e12dc13691b8569a409171a5
Amirates <jackyserberie@gmail.com>
2023-04-29 05:17:25 +0200

    Built getComputerChoice function

---
+++ main.js
@@ -0,0 +1,13 @@
+//create choices Array
+const choices = ["rock", "paper", "scissors"];
+
+//Function get computer choice 
+//Computer can choose between rock, paper and scissors and randomly returns these values as a string. 
+
+function getComputerChoice(){
+
+   return randomNumber = choices[Math.floor(Math.random()*choices.length)];
+
+}
+
+//Playerselection
